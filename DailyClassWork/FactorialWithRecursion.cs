ec2b9201a899a0338f883ed5ebaf90dc8bb99a9e
BootCampBL <satyabhandari43@gmail.com>
2021-09-24 18:33:26 +0530

    [satya] added- method to find Factorial of number with recursion

---
+++ DailyClassWork/FactorialWithRecursion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DailyClassWork
+{
+        class FactorialWithRecursion
+        {
+            static void Main(String[] args)
+            {
+                Console.WriteLine("Please enter factorial number");
+                int factNumber = Int32.Parse(Console.ReadLine());
+
+                int result = FactorialCalculation(factNumber);
+                Console.WriteLine("Result is :{0}", result);
+            }
+            static int fact = 1;
+            private static int FactorialCalculation(int factNumber)
+            {
+                if (factNumber > 0)
+                {
+                    fact = factNumber * FactorialCalculation(factNumber - 1);
+                }
+                return fact;
+            }
+        }
+    }
+}
+
