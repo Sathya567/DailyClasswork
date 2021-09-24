c6d62801dfd3fb502a4e0181c45fdeeb2602a425
BootCampBL <satyabhandari43@gmail.com>
2021-09-24 18:32:55 +0530

    [satya] added- method to find Factorial of number

---
+++ DailyClassWork/Factorial.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DailyClassWork
+{
+    class Factorial
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("please provide factorial number");
+            int FactNumber = Int32.Parse(Console.ReadLine());
+            int FactResult = 1;
+
+            //for ex-5!=1*2*3*4*5
+            for (int i = 1; i <= FactNumber; i++)
+            {
+                FactResult = FactResult * i;
+            }
+            Console.WriteLine("{0}! is {1}", FactNumber, FactResult);
+        }
+    }
+}
+   
