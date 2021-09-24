611f820680c049a1aef22d1caa1a623b85a55891
BootCampBL <satyabhandari43@gmail.com>
2021-09-24 18:34:16 +0530

    [satya] added- method to Solve A pattern with asterisks

---
+++ DailyClassWork/Apattern.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DailyClassWork
+{
+        class Apattern
+        {
+            static void Main(string[] args)
+            {
+                int i, j;
+
+                for (i = 1; i <= 5; i++)
+                {
+                    for (j = 1; j <= 4; j++)
+                    {
+                        if (j == 1 || j == 4 || i == 1 || i == 3)
+                        {
+                            Console.Write("*");
+                        }
+                        else
+                        {
+                            Console.Write(" ");
+                        }
+                    }
+                    Console.WriteLine();
+
+                }
+                Console.ReadLine();
+            }
+        }
+
+
+    
+}
+
