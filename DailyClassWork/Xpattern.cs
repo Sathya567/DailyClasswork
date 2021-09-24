59319bdc3a77baa7ba8693d0839cdc3502a2fbf6
BootCampBL <satyabhandari43@gmail.com>
2021-09-24 18:40:51 +0530

    [satya] added- method to Solve X pattern with asterisks

---
+++ DailyClassWork/Xpattern.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DailyClassWork
+{
+    class Xpattern
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (i == j || i + j == 5 - 1)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+   
