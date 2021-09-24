f4eee8935996121a9d3adbfdb2ce5a4c3ea44106
BootCampBL <satyabhandari43@gmail.com>
2021-09-24 18:40:08 +0530

    [satya] added- method to Solve Z pattern with asterisks

---
+++ DailyClassWork/Zpattern.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DailyClassWork
+{
+    class Zpattern
+    {
+        static void Main(String[] args)
+        {
+            int i, j;
+            for (i = 1; i <= 5; i++)
+            {
+                for (j = 1; j < 5; j++)
+                {
+                    if (i == 1 || i == 5 || i + j == 6)
+                        Console.Write("*");
+                    else
+                        Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
